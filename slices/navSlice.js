e9e20a0803bf175a97945bae91a555993faff233
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-16 22:01:41 -0400

    add selectors to enable the redux navSlice to fetch information from the rest of the app

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -24,3 +24,8 @@ export const navSlice = createSlice({
 
 // destructure - this line enables the rest of the app to access these objects
 export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions
+
+// selectors - enable the redux store to fetch information from the rest of the app
+export const selectOrigin = (state) => state.nav.origin
+export const selectDestination = (state) => state.nav.destination
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation
