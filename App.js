b66d0a00c1794f15db9bed457be0f16fc01543e7
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-18 21:28:38 -0400

    import map screen and create stack screen with map screen

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Provider } from "react-redux"
 import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { store } from "./store"
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 
 
 export default function App() {
@@ -22,6 +24,16 @@ const Stack = createStackNavigator()
           <Stack.Screen 
             name="HomeScreen"
             component={HomeScreen}
+            options={{
+              headerShown: false,
+            }}
+          />
+           <Stack.Screen 
+            name="MapScreen"
+            component={MapScreen}
+            options={{
+              headerShown: false,
+            }}
           />
         </Stack.Navigator>
         <HomeScreen />
