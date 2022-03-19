23cd17f037f3778405f2d2c092c214105b2b0da3
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-18 20:26:19 -0400

    add object array, FlatList tag and keyExtractor, render item array on the screen

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -1,11 +1,33 @@
-import { Text, View } from 'react-native'
 import React from 'react'
+import { FlatList, Text, View, TouchableOpacity } from 'react-native'
+
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: require('../assets/uber.png'),
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order food",
+        image: require('../assets/uber.png'),
+        screen: "EatsScreen",
+    }
+]
 
 const NavOptions = () => {
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
+    <FlatList 
+     data={data}
+     keyExtractor={(item) => item.id}
+     renderItem={({ item }) => (
+       <TouchableOpacity>
+         <Text>{item.title}</Text>
+       </TouchableOpacity>
+    )}
+  />
   )
 }
 
