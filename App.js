8819ce52f6ef6f7821a63365c3a9c326a482f61f
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-18 21:26:48 -0400

    install react gesture handler, navigation, and stack

--- App.js
+++ App.js
@@ -4,13 +4,30 @@ import { Provider } from "react-redux"
 import HomeScreen from './screens/HomeScreen';
 import { store } from "./store"
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 
 export default function App() {
+
+const Stack = createStackNavigator()
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
+       <NavigationContainer>
+        <SafeAreaProvider>
+        <GestureHandlerRootView>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="HomeScreen"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
         <HomeScreen />
-      </SafeAreaProvider>
+        </GestureHandlerRootView>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
