c939347d5bbc03432f65c777cdfe3319aaa04153
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-18 20:01:48 -0400

    add styles to view - padding 5

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -6,7 +6,7 @@ import tw from "tailwind-react-native-classnames"
 const HomeScreen = () => {
   return (
     <SafeAreaView style={tw `bg-white h-full`}>
-      <View>
+      <View style={tw `p-5`}>
         <Image 
           style={{width: 100, height: 100, resizeMode: "contain"}}
           source={require('../assets/uber.png')}
