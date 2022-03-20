68008c0885cee11e6c1830d72be21cf5add30b52
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-20 18:55:55 -0400

    add edgePadding to mapRef to zoom out the map

--- components/Map.jsx
+++ components/Map.jsx
@@ -16,7 +16,9 @@ const mapRef = useRef(null)
 useEffect(() => {
   if (!origin || !destination) return;
   // zoom & fit to markers
-  mapRef.current.fitToSuppliedMarkers(['origin', 'destination'])
+  mapRef.current.fitToSuppliedMarkers(['origin', 'destination'], {
+      edgePadding: { top: 50, right: 50, bottom: 50, left: 50 }
+  })
 }, [origin, destination])
 
   return (
