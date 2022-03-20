e0dc55b0023b3a66ba18c7275395f679fd53f5bd
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-20 18:05:12 -0400

    add opacity to disable options and enable them only after typing the destination

--- components/NavOptions.jsx
+++ components/NavOptions.jsx
@@ -3,6 +3,8 @@ import { FlatList, Text, View, TouchableOpacity, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import { Icon } from "react-native-elements"
 import { useNavigation } from '@react-navigation/native'
+import { useSelector } from 'react-redux'
+import { selectOrigin } from '../slices/navSlice'
 
 
 const data = [
@@ -24,6 +26,7 @@ const data = [
 const NavOptions = () => {
 
 const navigation = useNavigation()
+const origin = useSelector(selectOrigin)
 
   return (
     <FlatList 
@@ -34,8 +37,9 @@ const navigation = useNavigation()
        <TouchableOpacity
          onPress={() => navigation.navigate(item.screen)}
          style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+         disabled={!origin}
        >
-        <View>
+        <View style={tw `${!origin && 'opacity-20'}`}>
             <Image 
                 style={{width: 100, height: 100, resizeMode: "contain"}}
                 source={item.image}
