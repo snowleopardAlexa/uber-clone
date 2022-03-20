c2b4ef84e3dd89762676472cfac403785a0e234b
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-19 21:21:10 -0400

    add styles to button, add image and style text inside EatsScreen

--- screens/EatsScreen.jsx
+++ screens/EatsScreen.jsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, Button } from 'react-native'
 import React from 'react'
-
+import tw from "tailwind-react-native-classnames"
 
 const EatsScreen = () => {
   return (
-    <View>
-      <Text>Hello! I'm Eats Screen!</Text>
-      <Text>You've been naughty that's why you can't order food!</Text>
-      <Text>Go back to Home Screen!</Text>
+    <SafeAreaView style={tw `bg-white h-full`}>
+    <View style={tw `flex items-center m-auto`}>
+      <Image 
+        style={{width: 100, height: 100, resizeMode: "contain"}}
+        source={require('../assets/salad.png')}
+      />
+      <Text style={tw `text-xl mt-4`}>Hello! I'm Eats Screen!</Text>
+      <Text style={tw `text-lg text-center p-2`}>You've been naughty that's why you can't order food!</Text>
     </View>
+    <View style={tw `bg-black p-2 w-40 rounded mx-auto`}>
+    <Button
+     title="GO BACK"
+     color="#fff"
+    />      
+    </View>
+  
+    </SafeAreaView>
   )
 }
 
