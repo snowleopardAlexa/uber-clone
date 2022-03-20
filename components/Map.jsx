b38b74069524d49ada91585854fd93ddac20e377
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-20 12:41:00 -0400

    add map view to map component

--- components/Map.jsx
+++ components/Map.jsx
@@ -1,11 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import MapView, { Marker } from "react-native-maps"
+import tw from 'tailwind-react-native-classnames'
 
 const Map = () => {
   return (
-    <View>
-      <Text>I am a map</Text>
-    </View>
+    <MapView
+      style={tw `flex-1`}
+      initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+      }}
+    />
   )
 }
 
