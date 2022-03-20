50cacc233c592ef5ed09035409f5fdae95a22cb8
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-20 18:08:25 -0400

    add stack navigator and stack screen

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -3,15 +3,29 @@ import { StyleSheet, View } from 'react-native'
 import tw from "tailwind-react-native-classnames"
 import Map from '../components/Map'
 import MapView from "react-native-maps"
+import { createStackNavigator } from '@react-navigation/stack'
 
 
 const MapScreen = () => {
+
+  const Stack = createStackNavigator()
+
   return (
     <View>
       <View style={tw `h-1/2`}>
         <Map />
       </View>
-      <View style={tw `h-1/2`}></View>
+      <View style={tw `h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen 
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
