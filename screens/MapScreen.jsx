4421bde85d4357cea15c9735881bffc30c132ade
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-20 12:30:30 -0400

    style map screen and create map component

--- screens/MapScreen.jsx
+++ screens/MapScreen.jsx
@@ -1,11 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import tw from "tailwind-react-native-classnames"
 
 
 const MapScreen = () => {
   return (
     <View>
       <Text>I am MapScreen</Text>
+      <View style={tw `h-1/2`}></View>
+      <View style={tw `h-1/2`}></View>
     </View>
   )
 }
