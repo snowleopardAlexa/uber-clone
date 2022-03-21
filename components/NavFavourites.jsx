f79f6a086303fa97b714fde0a1d8e141d084974a
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-20 20:21:22 -0400

    add icons home and work and style them

--- components/NavFavourites.jsx
+++ components/NavFavourites.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { FlatList } from 'react-native-gesture-handler'
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler'
+import { Icon } from 'react-native-elements/dist/icons/Icon'
+import tw from 'tailwind-react-native-classnames'
 
 const data = [
     {
@@ -22,9 +24,19 @@ const NavFavourites = () => {
     <FlatList 
       data={data}
       keyExtractor={(item) => item.id}
-      renderItem={({ item }) => (
-          <TouchableOpacity>
-              <Text>Hello</Text>
+      renderItem={({ item: { location, destination, icon } }) => (
+          <TouchableOpacity style={tw `flex-row items-center p-5`}>
+             <Icon 
+               style={tw `mr-4 rounded-full bg-gray-300 p-3`}
+               name={icon}
+               type="ionicon"
+               color="white"
+               size={18}
+             />
+             <View>
+                 <Text style={tw `font-semibold text-lg`}>{location}</Text>
+                 <Text style={tw `text-gray-500`}>{destination}</Text>
+             </View>
           </TouchableOpacity>
       )}
     />
